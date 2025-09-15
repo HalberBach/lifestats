e66695378b532b3aedcc86833f1a9d2047da8b77
HalberBach <nilsjogie@gmail.com>
2025-09-15 16:40:38 +0200

    Landingpage Redesigned

--- src/components/start/StartHeader.vue
+++ src/components/start/StartHeader.vue
@@ -7,8 +7,11 @@ const router = useRouter()
 
 <template>
   <div class="flex justify-between items-center pb-4 border- border-b">
-    <div>
-      <h1>LOGO</h1>
+    <div class="flex items-center gap-4">
+      <div class=" h-6 w-6 items-center justify-center ">
+        <img src="@/assets/icon4.png" alt="Logo" />
+      </div>
+      <h1 class="font-bold text-2xl">Lifestats</h1>
     </div>
     <div>
       <Button class="mr-3" variant="ghost" @click="router.push('/login')">Log In</Button>
